fc803e53968b1277bc761303b41a5cb7af5de4cd
Jesse Wright <jesse.wright@utexas.edu>
2017-03-23 04:50:41 -0500

    Added SQLAlchemy setup in __init__.py as shown here: http://flask-sqlalchemy.pocoo.org/2.2/quickstart/#a-minimal-application.

--- idb/__init__.py
+++ idb/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/phase1.db'
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
     return "walter is cool"
-    
