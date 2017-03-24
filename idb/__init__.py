2521f58b154567689275be3414e4019a0ede6a68
Jesse Wright <jesse.wright@utexas.edu>
2017-03-23 21:21:09 -0500

    Added all models in models.py and verified correctness in new database. Added new database with test data. Added database.py to handle database creation (mainly included for reference).

--- idb/__init__.py
+++ idb/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/phase1.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/phase1.db'
 db = SQLAlchemy(app)
 
 
--- idb/models.py
+++ idb/models.py
@@ -1,81 +1,124 @@
 from idb import db
-from sqlalchemy import Column, String, Integer, Date, ForeignKey, Table
+from sqlalchemy import Column, String, Integer, Date, ForeignKey, Table, UniqueConstraint, Float
+
+# These below allow for many-to-many relationships - see http://flask-sqlalchemy.pocoo.org/2.2/models/
+
+# Many-to-many relationship between artists and works
+artists_works_relationship = db.Table('artists_works_relationship',
+                                      Column('artist_id', Integer, ForeignKey('artist.id'), primary_key=True),
+                                      Column('work_id', Integer, ForeignKey('work.id'), primary_key=True),
+                                      db.PrimaryKeyConstraint('artist_id', 'work_id'))
+
+# Many-to-many relationship between artists and styles
+artists_styles_relationship = db.Table('artists_styles_relationship',
+                                       Column('artist_id', Integer, ForeignKey('artist.id'), primary_key=True),
+                                       Column('style_id', Integer, ForeignKey('style.id'), primary_key=True),
+                                       db.PrimaryKeyConstraint('artist_id', 'style_id'))
+
+# Many-to-many relationship between artists and eras
+artists_eras_relationship = db.Table('artists_eras_relationship',
+                                     Column('artist_id', Integer, ForeignKey('artist.id'), primary_key=True),
+                                     Column('era_id', Integer, ForeignKey('era.id'), primary_key=True),
+                                     db.PrimaryKeyConstraint('artist_id', 'era_id'))
+
+# Many-to-many relationship between works and eras
+works_eras_relationship = db.Table('works_eras_relationship',
+                                   Column('work_id', Integer, ForeignKey('work.id'), primary_key=True),
+                                   Column('era_id', Integer, ForeignKey('era.id'), primary_key=True),
+                                   db.PrimaryKeyConstraint('work_id', 'era_id'))
+
+# Many-to-many relationship between styles and eras
+styles_eras_relationship = db.Table('styles_eras_relationship',
+                                    Column('style_id', Integer, ForeignKey('style.id'), primary_key=True),
+                                    Column('era_id', Integer, ForeignKey('era.id'), primary_key=True),
+                                    db.PrimaryKeyConstraint('style_id', 'era_id'))
 
 
 class Artist(db.Model):
+    """ A ``Model`` that houses information on artists. """
+    # TODO: Update data types to reflect actual content (e.g., lists, URIs, etc.)
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False, unique=True)
     dob = Column(Date)
     dod = Column(Date)
     nationality = Column(String(100))
+    country = Column(String(50))
     image = Column(String(100))
-    works = db.relationship('Work', backref='artist', lazy=True)
+
+    # TODO: Look into query/tables w.r.t. speed and efficiency and con
+    works = db.relationship('Work',
+                            secondary=artists_works_relationship,
+                            lazy=True,
+                            backref=db.backref('artists', lazy=True))
+
+    def __repr__(self):
+        """
+        :return: A formatted ``String`` representation of a given ``Artist``
+        """
+        return "{} ({}-{}), {}".format(self.name, self.dob, self.dod, self.nationality)
 
 
 class Work(db.Model):
+    """ A ``Model`` that houses information on artworks. """
     id = Column(Integer, primary_key=True)
-    title = Column(String(100), nullable=False, unique=True)
+    title = Column(String(100), nullable=False)
     date = Column(Date)
     medium = Column(String(100))
-    height = Column(Integer)
-    width = Column(Integer)
+    height = Column(Float)
+    width = Column(Float)
     colors = Column(String(100))
     image = Column(String(100))
-    artist = Column(Integer, ForeignKey('artist.id'))
+
+    def __repr__(self):
+        """
+        :return: A formatted ``String`` representation of a given ``Work``
+        """
+        return "\"{}\", by {} ({})".format(self.title, self.artist.name, self.date)
 
 
 class Style(db.Model):
+    """ A ``Model`` that houses information on artistic styles. """
     id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
     colors = Column(String(100))
-    averageAge = Column(Integer)
-    avgHeight = Column(Integer)
-    avgWidth = Column(Integer)
+    averageAge = Column(Float)
+    avgHeight = Column(Float)
+    avgWidth = Column(Float)
     images = Column(String(250))
 
-    # Allows for many-to-many relationships - see http://flask-sqlalchemy.pocoo.org/2.2/models/
-    # # Many-to-many relationship between artists and styles
-    artistsTable = Table('artists',
-                         Column('artist_id', Integer,
-                                ForeignKey('artist.id'), primary_key=True),
-                         Column('style_id', Integer,
-                                ForeignKey('style.id'), primary_key=True)
-                         )
-    artists = db.relationship('Artist', secondary=artistsTable, lazy=True,
+    artists = db.relationship(Artist,
+                              secondary=artists_styles_relationship,
+                              lazy=True,
                               backref=db.backref('styles', lazy=True))
+
+    def __repr__(self):
+        """
+        :return: A formatted ``String`` representation of a given ``Style``
+        """
+        return "{}".format(self.name)
 
 
 class Era(db.Model):
+    """ A ``Model`` that houses information on historical periods. """
     id = Column(Integer, primary_key=True)
+    name = Column(String(50))
     type = Column(String(50))
     countries = Column(String(250))
     media = Column(String(250))
 
-    # Many-to-many relationship between artists and eras
-    artistsTable = Table('artists',
-                         Column('artist_id', Integer,
-                                ForeignKey('artist.id'), primary_key=True),
-                         Column('era_id', Integer,
-                                ForeignKey('era.id'), primary_key=True)
-                         )
-    artists = db.relationship('Artist', secondary=artistsTable, lazy=True,
+    artists = db.relationship(Artist,
+                              secondary=artists_eras_relationship,
+                              lazy=True,
                               backref=db.backref('eras', lazy=True))
-
-    # Many-to-many relationship between works and eras
-    worksTable = Table('works',
-                       Column('work_id', Integer,
-                              ForeignKey('work.id'), primary_key=True),
-                       Column('era_id', Integer,
-                              ForeignKey('era.id'), primary_key=True)
-                       )
-    works = db.relationship('Work', secondary=worksTable, lazy=True,
+    works = db.relationship(Work, secondary=works_eras_relationship,
+                            lazy=True,
                             backref=db.backref('eras', lazy=True))
-
-    # Many-to-many relationship between styles and eras
-    stylesTable = Table('styles',
-                        Column('style_id', Integer,
-                               ForeignKey('style.id'), primary_key=True),
-                        Column('era_id', Integer,
-                               ForeignKey('era.id'), primary_key=True)
-                        )
-    styles = db.relationship('Style', secondary=stylesTable, lazy=True,
+    styles = db.relationship(Style, secondary=styles_eras_relationship,
+                             lazy=True,
                              backref=db.backref('eras', lazy=True))
+
+    def __repr__(self):
+        """
+        :return: A formatted ``String`` representation of a given ``Era``
+        """
+        return "{} ({})".format(self.name, self.type)
