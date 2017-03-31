71f370fbeaee12054358e93c715026578d1b3801
Walter Sagehorn <wsagehorn@gmail.com>
2017-03-30 21:33:47 -0500

    began work on dynamicizing

--- idb/views.py
+++ idb/views.py
@@ -4,7 +4,28 @@
 
 from flask import render_template
 from idb import app
+from idb.models import *
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/report')
+def report():
+    return render_template('report.html')
+
+
+
+@app.route('/artist')
+def artists():
+    artists = Artist.query.all()
+    return render_template('artists.html', artists=artists)
+
+@app.route('/artist/<int:id>')
+def artist(id):
+    artist = Artist.query.filter_by(id=id)
+    return render_template('artists.html', artist=artist)
