05f32f9423280923edc015d5f8c9f5066f7c57d6
Walter Sagehorn <wsagehorn@gmail.com>
2017-04-06 13:09:06 -0500

    work dimensions now strings

--- idb/queries.py
+++ idb/queries.py
@@ -24,6 +24,16 @@ def url_query_artist():
 def url_query_work():
     works, page_count  = query_work(request.args.to_dict())
     serialized_models = list(map(lambda x: x.serialize(), works))
+    for work in serialized_models:
+        work["height"] = str(work["height"])
+        work["width"] = str(work["width"])
+        work["depth"] = str(work["depth"])
+    return response(200, serialized_models, page_count)
+
+@app.route("/query_medium")
+def url_query_medium():
+    media, page_count  = query_medium(request.args.to_dict())
+    serialized_models = list(map(lambda x: x.serialize(), media))
     return response(200, serialized_models, page_count)
 
 def query_artist(args):
@@ -62,6 +72,30 @@ def query_work(args):
         works = date_filter(works, lambda x : x < int(args["date_before"]), "date")
     if "ascending" in args and args["ascending"] == "0":
         works = works[::-1]
+
+    page_count = int(ceil(len(works) / float(ITEMS_PER_PAGE)))
+
+    if "page" in args and args["page"]:
+        works = works[int(args["page"]) * ITEMS_PER_PAGE : (int(args["page"]) + 1) * ITEMS_PER_PAGE]
+
+    return works, page_count
+
+def query_medium(args):
+    works = Medium.query
+    if "order_by" in args and args["order_by"]:
+        if args["order_by"] == "name" or  args["order_by"] == "date":
+            works = works.order_by(args["order_by"])
+    works = works.all()
+    if "name_filter" in args and args["name_filter"]:
+        works = string_filter(works, "title", args["name_filter"])
+    if "date_after" in args and args["date_after"]:
+        works = date_filter(works, lambda x : x > int(args["date_after"]), "date")
+    if "date_before" in args and args["date_before"]:
+        works = date_filter(works, lambda x : x < int(args["date_before"]), "date")
+    if "ascending" in args and args["ascending"] == "0":
+        works = works[::-1]
+
+
 
     page_count = int(ceil(len(works) / float(ITEMS_PER_PAGE)))
 
