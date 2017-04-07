1a1954a75bd7e42857813a45b8355f857e0aa993
Walter Sagehorn <wsagehorn@gmail.com>
2017-04-06 19:25:21 -0500

    added medium query

--- idb/queries.py
+++ idb/queries.py
@@ -2,7 +2,6 @@
 # pylint: disable = invalid-name
 # pylint: disable = missing-docstring
 
-import re
 from math import ceil
 from flask import render_template, request, jsonify
 from idb import app
@@ -24,6 +23,7 @@ def url_query_artist():
 def url_query_work():
     works, page_count  = query_work(request.args.to_dict())
     serialized_models = list(map(lambda x: x.serialize(), works))
+    #convert to strings to avoid float conversion errors
     for work in serialized_models:
         work["height"] = str(work["height"])
         work["width"] = str(work["width"])
@@ -35,6 +35,8 @@ def url_query_medium():
     media, page_count  = query_medium(request.args.to_dict())
     serialized_models = list(map(lambda x: x.serialize(), media))
     return response(200, serialized_models, page_count)
+
+
 
 def query_artist(args):
     artists = Artist.query
@@ -81,28 +83,27 @@ def query_work(args):
     return works, page_count
 
 def query_medium(args):
-    works = Medium.query
+    media = Medium.query
     if "order_by" in args and args["order_by"]:
         if args["order_by"] == "name" or  args["order_by"] == "date":
-            works = works.order_by(args["order_by"])
-    works = works.all()
+            media = media.order_by(args["order_by"])
+    media = media.all()
     if "name_filter" in args and args["name_filter"]:
-        works = string_filter(works, "title", args["name_filter"])
+        media = string_filter(media, "name", args["name_filter"])
     if "date_after" in args and args["date_after"]:
-        works = date_filter(works, lambda x : x > int(args["date_after"]), "date")
+        media = date_filter(media, lambda x : x > int(args["date_after"]), "date")
     if "date_before" in args and args["date_before"]:
-        works = date_filter(works, lambda x : x < int(args["date_before"]), "date")
+        media = date_filter(media, lambda x : x < int(args["date_before"]), "date")
     if "ascending" in args and args["ascending"] == "0":
-        works = works[::-1]
+        media = media[::-1]
 
-
-
-    page_count = int(ceil(len(works) / float(ITEMS_PER_PAGE)))
+    page_count = int(ceil(len(media) / float(ITEMS_PER_PAGE)))
 
     if "page" in args and args["page"]:
-        works = works[int(args["page"]) * ITEMS_PER_PAGE : (int(args["page"]) + 1) * ITEMS_PER_PAGE]
+        media = media[int(args["page"]) * ITEMS_PER_PAGE : (int(args["page"]) + 1) * ITEMS_PER_PAGE]
 
-    return works, page_count
+    return media, page_count
+
 
 def date_filter(models, func, attribute):
     filtered = []
@@ -111,6 +112,8 @@ def date_filter(models, func, attribute):
             if func(getattr(model, attribute).year):
                 filtered.append(model)
         except:
+            # model is none or does not have attribute
+            # we don't care about these
             pass
     return filtered
 
