0403bd8750ef8999a88eaee9c9606dd8b9e71c41
Jesse Wright <jesse.wright@utexas.edu>
2017-04-11 20:54:44 -0500

    - database_tools.py:    * Updated to use simpler URI components by merging the fields for a socket and a Cloud SQL instance into one field to match the general structure of a URI for SQLAlchemy.    * Changed the default value of that new field to be blank so that development environments have to set fewer environment variables to test the app locally.    * Created central definition for environment variables' keys and the prefix used for those keys. - app.yaml:    * Updated to contain an environment variable with the DB option field that will point to the production site's local socket for the database. - __init__.py:    * Removed duplicate and outdated call to database_tools.py's build_db_connection_uri_string function (pretty sure it was an artifact from a bad merge). - test.db:    * Removed old SQLite database altogether since we no longer use it for the site, for development, or for testing. - tests.py:    * Updated use of fields for database_tools.py's build_db_connection_uri_string function.    * Added/modified comments about not having unit tests connect to the production database.

--- idb/__init__.py
+++ idb/__init__.py
@@ -7,8 +7,6 @@ app.config['TESTING'] = False
 app.config['DEBUG'] = True
 
 # Setup DB from environment variables or defaults
-
-app.config['SQLALCHEMY_DATABASE_URI'] = build_db_connection_uri_string()
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     build_db_connection_uri_string(use_env_vars=True,
