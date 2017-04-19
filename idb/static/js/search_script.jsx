f9898d4506902cca436da96d17de132e9a5878f3
James Graham <grahamjamesr7@gmail.com>
2017-04-18 21:30:41 -0500

    beginnings of actual searching

--- idb/static/js/search_script.jsx
+++ idb/static/js/search_script.jsx
@@ -3,7 +3,8 @@
 
 $('#searchForm').submit(function () {
 
-    
+
+    window.location.href = "/search_results/asdf";
 
  //keeps the page from reloading
  return false;
--- idb/views.py
+++ idb/views.py
@@ -148,6 +148,11 @@ def medium(id):
     return render_template('medium_instance.html', medium=medium,
                            colors=colors)
 
+
+@app.route('/search_results/<terms>')
+def search_results(terms):
+    return render_template('search_results.html',terms=terms)
+
 @app.route('/search/')
 def search():
     args = request.args.to_dict()
